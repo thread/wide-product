b39b5756469b3b41f44d028350ae04462fb5c92a
Alistair Lynn <arplynn@gmail.com>
2017-04-05 11:43:30 +0100

    Add initial README and setup

---
+++ README.rst
@@ -0,0 +1,33 @@
+wide-product
+============
+
+`wide-product` implements a partial, column-wise Khatri-Rao product. It is fast,
+and works on sparse matrices.
+
+It can be useful for engineering of cross-features for machine learning.
+
+Definition
+----------
+
+For a pair of scalars (~ one by one matrices), the wide product is
+multiplication:
+
+.. code:: python
+    wide_product ( [[a]], [[b]] ) = [[a * b]]
+
+Where matrices are constructed by *vertical stacking*, the product is row-wise:
+
+.. code:: python
+    wide_product ( vstack((A, B)), vstack((C, D)) ) =
+        vstack((wide_product(A, C),
+                wide_product(B, D)))
+
+Where matrices are constructed by *horizontal stacking*, the product contains
+all the products of the subcomponents up to permutation of columns:
+
+.. code:: python
+    wide_product ( hstack((A, B)), hstack((C, D)) ) =
+        hstack((wide_product(A, C),
+                wide_product(A, D),
+                wide_product(B, C),
+                wide_product(B, D)))
---
+++ setup.py
@@ -0,0 +1,61 @@
+import io
+
+from setuptools import setup, find_packages
+
+
+with io.open('README.rst', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+    name='wide-product',
+    version='0.0.1',
+    url='https://github.com/thread/wide-product',
+    description="Wide (partial Khatri-Rao) sparse matrix product",
+    long_description=long_description,
+
+    author="Thread Tech",
+    author_email="tech@thread.com",
+
+    keywords = (
+        'numpy',
+        'scipy',
+        'matrix',
+        'sparse',
+        'khatri-rao',
+        'product',
+        'science',
+        'feature',
+        'courgette',
+    ),
+    license='MIT',
+
+    zip_safe=False,
+
+    packages=find_packages(),
+
+    classifiers=(
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Software Development :: Libraries',
+    ),
+
+    install_requires=(
+        'numpy',
+        'scipy',
+    )
+)
